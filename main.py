1beada54895f25a56218c57f25ba80806aeba69f
Rogério Farias Otto <64393065+rogeriootto@users.noreply.github.com>
2021-04-24 23:42:53 -0300

    ARRUMADO E LIMPINHO

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+## //----------------------------- PRECISA DISSO PRA RODAR O BOT -----------------------------//
+
 ## PRECISA pip install -U discord.py[voice]
 ## https://www.youtube.com/watch?v=ml-5tXRmmFk
 ## pip install youtube_dl
 ## git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg
 ## pip install pytube3
 
+## //-------------------------------------------------------------------//
+
+
+## //----------------------------- IMPORTS -----------------------------//
 import discord
 import os
 from random import choice
@@ -12,6 +18,12 @@ from discord.ext import tasks
 import random
 import drawfunction
 import youtubeshit
+import json
+
+## //-------------------------------------------------------------------//
+
+
+## //----------------------------- COISAS IMPORTANTES PARA O BOT COMEÇAR A RODAR -----------------------------//
 
 client = commands.Bot(command_prefix='-')
 status = ['Jogando Galo de Rinha em C', 'Ultra trolha', '∞³ > ∞²', '-gHelp for Help']
@@ -26,6 +38,20 @@ async def on_ready():
     change_status.start()
     print('O {0.user} TA ON RAPAZIADA'.format(client))
 
+## //--------------------------------------------------------------------------------------------------------//
+
+
+## //----------------------------- AJUDA -----------------------------//
+
+@client.command(pass_context=True)
+async def gHelp(ctx):
+    await ctx.send('```\nCOMANDOS:\n-gJoin -- Entra no canal\n-gLeave -- Sai do canal\n-gPlay [url] -- toca musica do yt (favor não colocar livestream)\n-gVUKVUK\n-gGALO\n-gHelp -- Tu recem digitou essa porra pra q tu vai querer usar isso aqui dnv wtf\n```')
+    await ctx.send('```\n**Memes**\n-kayara\n-among\n-ultra\n-picotinha\n-salazar\n-murilo\n-nehmole -- n eh mole\n-matematica\n-getTomorrowsDate\n-todaywasagoodday```')
+
+## //-----------------------------------------------------------------//
+
+
+## //----------------------------- FUNÇÕES PARA O YOUTUBE -----------------------------//
 # Playa musica
 @client.command(pass_context=True)
 async def gPlay(ctx, url: str):
@@ -49,26 +75,6 @@ async def gPause(ctx):
         voice.pause()
     else:
         await ctx.send('NAO TA TOCANDO NADA PQ TU QUER PAUSAR?????')
-
-@client.command(pass_context=True)
-async def gGALO(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    voice.play(discord.FFmpegPCMAudio("mp3/galo.mp3"))
-
-@client.command(pass_context=True)
-async def gKNOCK(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    voice.play(discord.FFmpegPCMAudio("mp3/knock.mp3"))
-
-@client.command(pass_context=True)
-async def gVUKVUK(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    voice.play(discord.FFmpegPCMAudio("mp3/vukvuk.mp3"))
-    await ctx.send('É VUK VUK POR 10H, VUK VUK VUK VUK VUK')
-
-@client.command(pass_context=True)
-async def umapeca(ctx):
-    await ctx.send('https://cdn.discordapp.com/attachments/238065523961757696/790033127619821608/unknown.png')
 
 @client.command(pass_context=True)
 async def gdPlay(ctx):
@@ -112,20 +118,79 @@ async def gLeave(ctx):
     else:
         await ctx.send('EU NÃO TO NO CANAL PORRA')
 
-# Ajuda
-@client.command(pass_context=True)
-async def gHelp(ctx):
-    await ctx.send(
-        '```\nCOMANDOS:\n-gJoin -- Entra no canal\n-gLeave -- Sai do canal\n-gPlay [url] -- toca musica do yt (favor não colocar livestream)\n-gVUKVUK\n-gGALO\n-gHelp -- Tu recem digitou essa porra pra q tu vai querer usar isso aqui dnv wtf\n```')
-    await ctx.send(
-        '```\n**Memes**\n-kayara\n-among\n-ultra\n-picotinha\n-salazar\n-murilo\n-nehmole -- n eh mole\n-matematica\n-getTomorrowsDate\n-todaywasagoodday```')
+## //-------------------------------------------------------------------------------------------//
 
-# DRAW BOT
+
+## //----------------------------- MEMES COM AUDIO QUE O BOT TOCA -----------------------------//
+
+@client.command(pass_context=True)
+async def gGALO(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    voice.play(discord.FFmpegPCMAudio("mp3/galo.mp3"))
+
+@client.command(pass_context=True)
+async def gKNOCK(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    voice.play(discord.FFmpegPCMAudio("mp3/knock.mp3"))
+
+@client.command(pass_context=True)
+async def gVUKVUK(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    voice.play(discord.FFmpegPCMAudio("mp3/vukvuk.mp3"))
+    await ctx.send('É VUK VUK POR 10H, VUK VUK VUK VUK VUK')
+
+## //--------------------------------------------------------------------------------------------//
+
+
+## //----------------------------- FUNÇÕES PARA O SISTEMA DO BANCO ------------------------------//
+
+
+
+
+## //--------------------------------------------------------------------------------------------//
+
+
+## //----------------------------- FUNÇÕES PARA O MEME DOS DRAW ---------------------------------//
+
 @client.command(pass_context=True)
 async def gDraw(ctx):
 
     embed, file = drawfunction.randDraw(ctx)
     await ctx.send(embed=embed, file=file)
+
+@client.command(pass_context=True)
+async def jesus(ctx):
+
+    embed, file = drawfunction.jesus(ctx)
+    await ctx.send(embed=embed, file=file)
+
+@client.command(pass_context=True)
+async def ultratrolha(ctx):
+
+    embed, file = drawfunction.ultratrolha(ctx)
+    await ctx.send(embed=embed, file=file)
+
+@client.command(pass_context=True)
+async def among(ctx):
+
+    embed, file = drawfunction.among(ctx)
+    await ctx.send(embed=embed, file=file)
+
+@client.command(pass_context=True)
+async def galosniper(ctx):
+
+    embed, file = drawfunction.galosniper(ctx)
+    await ctx.send(embed=embed, file=file)
+
+## //---------------------------------------------------------------------------------------------------//
+
+
+## //----------------------------- ALGUNS MEMES QUE SÓ PRINTAM NO DISCORD -----------------------------//
+
+
+@client.command(pass_context=True)
+async def umapeca(ctx):
+    await ctx.send('https://cdn.discordapp.com/attachments/238065523961757696/790033127619821608/unknown.png')
 
 @client.command(pass_context=True)
 async def kayara(ctx):
@@ -154,30 +219,6 @@ async def nehmole(ctx):
         'https://cdn.discordapp.com/attachments/588545039777660949/829071161186189352/d317a2456aad6e995454ea93a96ce3ee3e3f9f32a73ecaf16b5b6dd30eb0736b_1.png')
 
 @client.command(pass_context=True)
-async def jesus(ctx):
-
-    embed, file = drawfunction.jesus(ctx)
-    await ctx.send(embed=embed, file=file)
-
-@client.command(pass_context=True)
-async def ultratrolha(ctx):
-
-    embed, file = drawfunction.ultratrolha(ctx)
-    await ctx.send(embed=embed, file=file)
-
-@client.command(pass_context=True)
-async def among(ctx):
-
-    embed, file = drawfunction.among(ctx)
-    await ctx.send(embed=embed, file=file)
-
-@client.command(pass_context=True)
-async def galosniper(ctx):
-
-    embed, file = drawfunction.galosniper(ctx)
-    await ctx.send(embed=embed, file=file)
-
-@client.command(pass_context=True)
 async def malloc(ctx):
     await ctx.send('https://twitter.com/c0dehard/status/1327718161848872960')
 
@@ -200,4 +241,8 @@ async def getTomorrowsDate(ctx):
     await ctx.send(
         'https://cdn.discordapp.com/attachments/597584201818046515/829030594796191845/48382146_333240393936526_3546234963072385024_n.png')
 
-client.run(token)
+## //------------------------------------------------------------------//
+
+## //----------------------------- TOKEN -----------------------------//
+client.run(TOKEN)
+## //-----------------------------------------------------------------//
